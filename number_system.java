2916e85c1417b28cf138ff3a8e75f394f384dc80
JeffAntony <142667486+JeffAntony@users.noreply.github.com>
2024-01-19 10:35:40 +0530

    number_system in java
    conversion of decimal to binary
    conversion of binary to decimal

---
+++ number_system.java
@@ -0,0 +1,47 @@
+
+// number system :
+//  decimal and binary system
+// converting binary to decimal
+//import java.util.*;
+// public class Tree_ {
+//     public static void main(String[] args) {
+//
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("enter the value");
+//         int num =sc.nextInt();
+//         int ans =0;
+//         int p =1;
+//         while(num>0)
+//         {
+//             int digit= num%10;
+//             ans =ans+(digit*p);
+//             num=num/10;
+//             p*=2;
+//
+//         }
+//         System.out.println(ans);
+//     }
+//}
+
+
+// decimal to conversion
+// import java.util.*;
+// public class Tree_ {
+//     public static void main(String[] args)
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the value");
+//         int num = sc.nextInt();
+//         int ans=0;
+//         int p=1;
+//         while(num>0)
+//         {
+//             int rem =num%2;
+//             ans+=(rem*p);
+//             p*=10;
+//             num=num/2;
+//         }
+//         System.out.println(ans);
+//     }
+//
+//     }
